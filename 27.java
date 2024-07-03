5b3e76412b06090a17845679a0693ce7b3d22a8a
tejaltatiwari <tejaltatiwar487@gmail.com>
2024-07-03 18:49:15 +0530

    Hackerrank code added

---
+++ 27.java
@@ -0,0 +1,45 @@
+// Circular Array Rotation
+// John Watson knows of an operation called a right circular rotation on an array of integers. One rotation operation moves the last array element to the first position and shifts all remaining elements right one. To test Sherlock's abilities, Watson provides Sherlock with an array of integers. Sherlock is to perform the rotation operation a number of times then determine the value of the element at a given position.
+
+// For each array, perform a number of right circular rotations and return the values of the elements at the given indices.
+
+import java.util.Scanner;
+
+class Solution {
+    public static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int q = sc.nextInt();
+        int[] num = new int[n];
+        for (int i = 0; i < n; i++) {
+            num[i] = sc.nextInt();
+        }
+        int[] queries = new int[q];
+        for (int i = 0; i < q; i++) {
+            queries[i] = sc.nextInt();
+        }
+
+        rotate(num, k);
+        for (int i = 0; i < q; i++) {
+            System.out.println(num[queries[i]]);
+        }
+    }
+}
