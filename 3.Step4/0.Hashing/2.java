a682a2a4f4d9fb59bbb82ebcdfb6ebb8990a5659
tejaltatiwari <tejaltatiwar487@gmail.com>
2024-07-09 18:35:35 +0530

    Hackerrank code added

--- 3.Step4/0.Hashing/2.java
+++ 3.Step4/0.Hashing/2.java
@@ -24,3 +24,35 @@
 //         System.out.println("Count of pairs: " + bruteForceCountPairs(arr, k));
 //     }
 // }
+
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    static int countPairsWithSum(int[] b, int k) {
+        int count = 0;
+        Map<Integer, Integer> seen = new HashMap<>();
+        
+        for (int j = 0; j < b.length; ++j) {
+            int complement = k - b[j];
+            if (seen.containsKey(complement)) {
+                count += seen.get(complement);// adding how many times complement is present
+            }
+            seen.put(b[j], seen.getOrDefault(b[j], 0) + 1);
+        }
+        
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] b = {1, 2, 3, 4,4, 5};
+        int k = 6;
+        int count = countPairsWithSum(b, k);
+        
+        System.out.println("Count of pairs with sum " + k + " is: " + count);
+    }
+}
